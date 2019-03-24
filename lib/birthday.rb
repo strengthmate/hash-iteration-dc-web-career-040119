397e42309dd1227136c4705510d8b148f53fa2e7
Matthew Eric <mf9646a@alumni.american.edu>
2019-03-24 15:37:57 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -8,7 +8,7 @@ def happy_birthday(birthday_kids)
   birthday_kids.each do |kids_name, age|
     if age < 14
       puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
-    elsif age >= 14
+    else
      puts "You are too old for this."
     end
   end
