89f842a6f57173c871cab62a277671c9b6267c49
Matthew Eric <mf9646a@alumni.american.edu>
2019-03-24 15:13:50 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -7,11 +7,5 @@
 # }
 
 def select_winner(passengers)
-  winner = ""
-  passengers.each do |suite, name|
-    if suite == :suite_a && name.start_with?("A")
-      winner = name
-    end
+  # add the code snippet here!
 end
-   
-  winner
